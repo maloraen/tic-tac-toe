7e2ccbac3fc8949dd005e9e7e7a8e5f3c9669771
maloraen <158532343+maloraen@users.noreply.github.com>
2025-05-29 12:53:44 -0500

    base code

---
+++ script.js
@@ -0,0 +1,17 @@
+const Gameboard = (function () {
+  let board = [];
+
+
+})();
+
+function Player(name, marker) {
+  return { name, marker };
+}
+
+const GameFlow = (function () {
+
+})();
+
+const displayController = (function () {
+
+})();
