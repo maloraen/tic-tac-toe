067fa9d3609429c67d386a2772a50f3805bb651a
maloraen <158532343+maloraen@users.noreply.github.com>
2025-05-31 02:47:07 -0500

    code cleanup

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const Gameboard = function () {
   let spaces = ["", "", "", "", "", "", "", "", ""]; // create board
-  const placeMarker = (space, marker) => (spaces[space - 1] = marker); // - 1 to line the spaces up properly with the 0-indexed array
+  const placeMarker = (space, marker) => (spaces[space] = marker); // place markers, adjusting for 0-indexing
   const printBoard = () => console.log(spaces); // for console version
 
   return { spaces, placeMarker, printBoard };
@@ -14,38 +14,32 @@ const GameFlow = (function (
   playerOneName = "Player One",
   playerTwoName = "Player Two"
 ) {
-  const board = Gameboard(); // get access to the Gameboard()
+  const board = Gameboard();
   let gameOver = false;
 
   // create the players
   const players = [
-    {
-      name: playerOneName,
-      marker: "X",
-    },
-    {
-      name: playerTwoName,
-      marker: "O",
-    },
+    { name: playerOneName, marker: "X" },
+    { name: playerTwoName, marker: "O", }
   ];
 
   let activePlayer = players[0];
-  const switchPlayerTurn = () =>
-    (activePlayer = activePlayer === players[0] ? players[1] : players[0]);
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+  
   const getActivePlayer = () => activePlayer;
 
   const printRound = () => {
     board.printBoard();
-    if (!gameOver) {
-      console.log(`${getActivePlayer().name}'s turn`);
-    } else {
-      console.log("Game Over!");
-    }
+    console.log(gameOver ? "Game Over!" : `${getActivePlayer().name}'s turn`);
   };
 
   const playRound = (space) => {
+    space--; // adjust for 0-index
     if (!gameOver) {
-      if (board.spaces[space - 1] == "") {
+      if (board.spaces[space] == "") {
         // if selected space is empty
         console.log(
           `Placing ${getActivePlayer().name}'s marker in space ${space}`
@@ -64,23 +58,20 @@ const GameFlow = (function (
   let winningPattern = [];
   const checkForWin = (player) => {
     const winPatterns = [
-      [0, 1, 2], // rows
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6], // columns
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8], // diagonals
-      [2, 4, 6],
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+      [0, 4, 8], [2, 4, 6]             // diagonals
+      
     ];
 
     // take the index of each space filled by current player's marker
     let indexes = [];
+    // TO DO: convert to .map() and .filter()
     for (let i = 0; i < 9; i++) {
       if (board.spaces[i] == player.marker) indexes.push(i);
     }
 
-    // check if each number of any pattern is in the indexes array
+    // check if any win pattern is in the indexes array
     let winner = false;
     winPatterns.forEach((pattern) => {
       if (pattern.every((index) => indexes.includes(index))) {
@@ -111,19 +102,19 @@ const GameFlow = (function (
 })();
 
 const displayControl = (function () {
-  const consoleBoard = GameFlow.board; // get access to board
-  const UIBoard = document.querySelector(".board"); // select board element for display
+  const consoleBoard = GameFlow.board;
+  const UIBoard = document.querySelector(".board");
 
   // highlight winning pattern
   function highlightWin() {
-    const winpattern = GameFlow.getWinningPattern(); // get access to the indexes of the winning pattern of markers
+    const winpattern = GameFlow.getWinningPattern();
     winpattern.forEach((i) => {
-      const space = document.querySelector(`.space${i + 1}`); // select each space with index adjusted for 0-indexing
+      const space = document.querySelector(`.space${i + 1}`);
       space.style.color = "aqua";
     });
   }
 
-  // make board red on a tie
+  // board turns red on a tie
   const redBoard = () => {
     const spaces = document.querySelectorAll(`div[class*="space"]`); // select all markers
     spaces.forEach((space) => {
@@ -135,18 +126,14 @@ const displayControl = (function () {
   const renderBoard = () => {
     UIBoard.innerHTML = ""; // clear board
 
-    // render spaces for each space in array
     consoleBoard.spaces.forEach((space, i) => {
-      // create the space divs
       const spaceElem = document.createElement("div");
       spaceElem.classList.add(`space${i + 1}`);
 
-      // add paragraph elements
       const pElem = document.createElement("p");
       pElem.innerText = `${space}`;
       spaceElem.appendChild(pElem);
 
-      // place markers and play game
       pElem.addEventListener("click", () => {
         GameFlow.playRound(i + 1);
         renderBoard();
@@ -155,11 +142,11 @@ const displayControl = (function () {
       UIBoard.appendChild(spaceElem); // add spaces to board
     });
 
-    // call highlightWin() or redBoard() after re-rendering, to re-apply style changes
+    // call highlightWin() or redBoard() *after* re-rendering, if game is over
     if (GameFlow.getGameStatus()) (GameFlow.getWinningPattern().length > 0) ? highlightWin() : redBoard();
   };
 
-  // reset board
+  // reset button to clear board and restart game
   const resetButton = document.querySelector(".reset");
   resetButton.addEventListener("click", () => {
     location.reload();
